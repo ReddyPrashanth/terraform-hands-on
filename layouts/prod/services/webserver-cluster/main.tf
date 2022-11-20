c63edebdd6e6783c0cfde3776afdd9f4f2c13990
prashanth sreepathi <iamsreepathi@gmail.com>
2022-11-20 10:15:54 -0600

    moved modules to a seperate git repository

--- layouts/prod/services/webserver-cluster/main.tf
+++ layouts/prod/services/webserver-cluster/main.tf
@@ -1,10 +1,9 @@
 provider "aws" {
   region = "us-east-2"
-  profile = "terraform"
 }
 
 module "webserver_cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  source = "github.com/ReddyPrashanth/terraform-modules//modules/services/webserver-cluster?ref=v0.0.1"
 
   cluster_name = "webservers-prod"
   db_remote_state_bucket = "terraform-up-and-running-state-psreepathi"
